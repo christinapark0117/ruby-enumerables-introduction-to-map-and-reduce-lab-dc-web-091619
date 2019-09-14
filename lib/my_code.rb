0f4307d07e41b3cdf3737032a5b3626f3783ffaf
Christina Park <christinapark0117@gmail.com>
2019-09-14 02:23:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,3 +21,14 @@ def map_to_double(source_array)
   end
   return source_array
 end
+
+def map_to_square(source_array)
+  index = 0
+  
+  while index < source_array.length do
+    source_array[index] = source_array[index] ** 2
+    index += 1
+  end
+  return source_array
+end
+
