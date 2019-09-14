298ca5b41b4d6eafafe014292b7d565dbcf831de
Christina Park <christinapark0117@gmail.com>
2019-09-14 01:57:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 def map_to_negativize(source_array)
   index = 0
   while index < source_array.length do
-    source_array[index] * -1
+    sourcesource_array[index] * -1
     counter += 1  
   end
 end  
