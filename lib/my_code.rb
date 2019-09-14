03207ea40ecd03df1d20b560c99ce38eb5bdee46
Christina Park <christinapark0117@gmail.com>
2019-09-14 02:21:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,17 @@ def map_to_no_change(source_array)
   index = 0
   
   while index < source_array.length do
-    source_array[index] = source_array[index] * -1
+    
+    index += 1
+  end
+  return source_array
+end
+
+def map_to_double(source_array)
+  index = 0
+  
+  while index < source_array.length do
+    source_array[index] = source_array[index] * 2
     index += 1
   end
   return source_array
